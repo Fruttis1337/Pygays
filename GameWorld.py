31342d6e5658e7d0945ec63f7ff1b53164d97e46
ReptileSD <50035939+ReptileSD@users.noreply.github.com>
2019-04-27 14:51:20 +1000

    Add files via upload

--- GameWorld.py
+++ GameWorld.py
@@ -1,26 +1,36 @@
 class World:
-    def __init__(self, platforms=[], coins=[], enemies=[], boss=[]):
+    def __init__(self, platforms=[], coins=[], enemies=[], boss=None):
         self.platforms = platforms
         self.coins = coins
         self.enemies = enemies
         self.boss = boss
 
-    def check_platform(self):
+    def check_platform(self, coords):
         for i in self.platforms:
-            if i.get_type:
+            if i.get_coords == coords:
+                i.start_count = True
+            if i.get_state():
                 i.lower_time()
         filter(lambda x: x.is_broken(), self.platforms)
 
-    def check_coins(self):
+    def check_coins(self, coords):
         for i in self.coins:
-            i.take()
-        filter(lambda x: x.is_taken, self.platforms)
+            i.take(coords)
+        filter(lambda x: not x.is_taken, self.platforms)
 
-    def check_enemies(self):
-        pass #сами делайте
+    def check_enemies(self, coords):
+        for i in self.enemies:
+            if coords == i.get_coords:
+                pass #вызвать консоль выбора
+        filter(lambda x: x.get_state(), self.enemies)
 
-    def check_boss(self):
-        pass #и это тоже
+    def check_boss(self, coords):
+        if self.boss is not None:
+            if coords == self.boss.get_coords:
+                pass #наносится урон игроку
+        if not self.boss.get_state():
+            self.boss = None
+
 
     def get_platforms(self):
         return [i.get_coords() for i in self.platforms]
@@ -32,4 +42,4 @@ class World:
         return [i.get_coords() for i in self.enemies]
 
     def get_boss(self):
-        return [i.get_coords() for i in self.boss]
+        return self.boss.get_coords()
